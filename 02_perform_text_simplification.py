5653d6e5612a68bc58f2664d1223e9cec6fed78c
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-07 10:44:49 +0100

    add data for saving converted texts as dataframe, and add use of new get_dataset method

--- 02_perform_text_simplification.py
+++ 02_perform_text_simplification.py
@@ -9,20 +9,33 @@ Prompts can have different components:
 - could have or not have the questions which have to be answerable given the text.
 """
 import os
+import pandas as pd
 import pickle
 from src.utils import init_adapter
 from src.constants import GPT_3_5_0613
 from constants import CERD
-from utils import get_key_from_model_name
+from utils import get_key_from_model_name, get_dataset
 
 
-def main(model_name: str, prompt_id: str, dataset_name: str, target_cefr_level: str, access_key: str):
+def main(model_name: str,
+         prompt_id: str,
+         dataset_name: str,
+         target_cefr_level: str,
+         access_key: str,
+         save_as_dataframe: bool = True,
+         ):
     adapter = init_adapter(model_name, access_key=access_key)
-    converted_texts = adapter.convert_texts(prompt_id, dataset_name, target_cefr_level)
+    dataset = get_dataset(dataset_name)[:10]  # the [:10] is TMP for preliminary experimetns.
+    converted_texts = adapter.convert_texts(prompt_id, dataset, target_cefr_level)
     output_path = f'data/output/{dataset_name}/{model_name}/{prompt_id}'
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     pickle.dump(converted_texts, open(os.path.join(output_path, f'converted_texts_{target_cefr_level}.pkl'), 'wb'))
+    if save_as_dataframe:
+        df_converted_texts = pd.DataFrame.from_dict(converted_texts, orient='index')
+        df_converted_texts = df_converted_texts.reset_index()
+        df_converted_texts = df_converted_texts.rename(columns={'index': 'text_id', 0: 'text'})
+        df_converted_texts.to_csv(os.path.join(output_path, f'df_converted_texts_{target_cefr_level}.csv'), index=False)
 
 
 if __name__ == '__main__':
