d5bb493e8b4ac9c18d7fc1883389888c21095711
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-07 16:27:18 +0100

    fix comment

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def get_key_from_model_name(model_name: str) -> str:
         api_key = data['key']
         return api_key
     if model_name in PRETRAINED_MODEL_NAMES:
-        # with open('/home/lb990/.keys/hf_access_token.json', 'r') as f:  # This is for my local machine
+        # with open('/home/luca/.keys/hf_access_token.json', 'r') as f:  # This is for my local machine
         with open('/home/lb990/.keys/hf_access_token.json', 'r') as f:  # This is for the server
             data = json.load(f)
         access_token = data['key']
