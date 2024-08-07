c3402ba46ed1407d233e99e41816585fffb2fb50
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-07 16:26:44 +0100

    add constants for names models from HF

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,20 @@
-# Names of pretrained models available on Hugging Face
+# Names of the models used which are available on Hugging Face
 GEMMA_2B = 'gemma_2b'
 GEMMA_7B = 'gemma_7b'
 LLAMA_3_8B = 'llama3_8b'
+LLAMA_3_1_8B = 'llama3_1_8b'
+MINICPM_v2_6 = 'minicpm_v2_6'
 MISTRAL_7B_v02 = 'mistral_v02_7b'
+MISTRAL_7B_v03 = 'mistral_v03_7b'
 
 PRETRAINED_MODEL_NAMES = {
     GEMMA_2B: "google/gemma-2b-it",
     GEMMA_7B: "google/gemma-7b-it",
     LLAMA_3_8B: "meta-llama/Meta-Llama-3-8B-Instruct",
-    MISTRAL_7B_v02: "mistralai/Mistral-7B-Instruct-v0.2"
+    LLAMA_3_1_8B: "meta-llama/Meta-Llama-3.1-8B-Instruct",
+    MINICPM_v2_6: "openbmb/MiniCPM-V-2_6",
+    MISTRAL_7B_v02: "mistralai/Mistral-7B-Instruct-v0.2",
+    MISTRAL_7B_v03: "mistralai/Mistral-7B-Instruct-v0.3",
 }
 
 # Names of OpenAI models
