02e447b6c7c73d9b549c4c028b47dafeafe20235
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-09 14:53:19 +0100

    add comment to describe script

--- 01_preapare_word_lists.py
+++ 01_preapare_word_lists.py
@@ -1,3 +1,14 @@
+"""
+This script prepares the word lists for different CEFR levels and stores them in the 'data/input/' directory.
+At this stage it only works on the olp_en_cefrj vocabulary profile (as taken from
+https://github.com/openlanguageprofiles/olp-en-cefrj).
+Possibly TODO: get the word lists from Cambridge Assessment.
+These are some useful links if found:
+- from pre-A1 to A2: https://www.cambridgeenglish.org/images/149681-yle-flyers-word-list.pdf
+- A2: https://www.cambridgeenglish.org/Images/506886-a2-key-2020-vocabulary-list.pdf
+- B1: https://www.cambridgeenglish.org/Images/506887-b1-preliminary-2020-vocabulary-list.pdf
+The second and third are also the ones which we have linked in our github repo.
+"""
 import pandas as pd
 
 
