caed7d60a65cac763616fbe5954d233cedc44af5
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-26 13:00:31 +0100

    rename variable name

--- 03_evaluation_readability.py
+++ 03_evaluation_readability.py
@@ -21,11 +21,11 @@ def readability_evaluation(dataset_name: str, model_name: str, prompt_id: str, t
 
 
 if __name__ == '__main__':
-    for dataset_name in [CERD, CAM_MCQ]:
-        for model_name in [GEMMA_2B, GEMMA_7B, LLAMA_3_8B, GPT_4o_240806, GPT_4o_MINI_240718]:
-            for prompt_id in ['01', '02', '11', '12', 'w01', 'w02']:
-                if prompt_id in ['w01', 'w02']:
-                    readability_evaluation(dataset_name, model_name, prompt_id, 'A1')
+    for dataset_name_param in [CERD, CAM_MCQ]:
+        for model_name_param in [GEMMA_2B, GEMMA_7B, LLAMA_3_8B, GPT_4o_240806, GPT_4o_MINI_240718]:
+            for prompt_id_param in ['01', '02', '11', '12', 'w01', 'w02']:
+                if prompt_id_param in ['w01', 'w02']:
+                    readability_evaluation(dataset_name_param, model_name_param, prompt_id_param, 'A1')
                 else:
                     for target_level in CEFR_LEVELS[:-1]:
-                        readability_evaluation(dataset_name, model_name, prompt_id, target_level)
+                        readability_evaluation(dataset_name_param, model_name_param, prompt_id_param, target_level)
