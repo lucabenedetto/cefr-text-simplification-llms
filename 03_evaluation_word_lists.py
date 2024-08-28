b221006fe739138a8046a37f46beab1629b7e894
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-28 09:52:21 +0100

    add code to evaluate all models on prompts 01...12

--- 03_evaluation_word_lists.py
+++ 03_evaluation_word_lists.py
@@ -11,6 +11,7 @@ from src.constants import (
 )
 from constants import CERD, CAM_MCQ
 from src.evaluators.word_list import WordListEvaluator
+from src.utils_plotting import line_plot_word_lists_count
 
 
 def word_list_evaluation(dataset_name: str, model_name: str, prompt_id: str, target_level: str):
@@ -41,3 +42,27 @@ def compute_and_save_word_list_evaluation():
 
 if __name__ == '__main__':
     compute_and_save_word_list_evaluation()
+
+    # this is to plot the results
+    for dataset_name_param in [CERD, CAM_MCQ, 'aggregate']:
+        for model_name_param in [GEMMA_2B, GEMMA_7B, LLAMA_3_8B, GPT_4o_240806, GPT_4o_MINI_240718]:
+            for prompt_id_param in ['01', '02', '11', '12']:
+                if dataset_name_param in [CERD, CAM_MCQ]:
+                    word_lists_count_per_level = [
+                        pd.read_csv(f'data/evaluation/{dataset_name_param}/{model_name_param}/word_lists_{prompt_id_param}_target_{level}.csv')
+                        for level in CEFR_LEVELS[:-1]
+                    ]
+                    line_plot_word_lists_count(word_lists_count_per_level, f"{dataset_name_param} | {model_name_param} | {prompt_id_param}", f"{dataset_name_param}_{model_name_param}_{prompt_id_param}", figsize=(3, 2.1))
+                else:
+                    word_lists_count_per_level = [
+                        pd.read_csv(f'data/evaluation/{CERD}/{model_name_param}/word_lists_{prompt_id_param}_target_{level}.csv')
+                        for level in CEFR_LEVELS[:-1]
+                    ]
+                    word_lists_count_per_level_cam_mcq = [
+                        pd.read_csv(f'data/evaluation/{CAM_MCQ}/{model_name_param}/word_lists_{prompt_id_param}_target_{level}.csv')
+                        for level in CEFR_LEVELS[:-1]
+                    ]
+                    for idx in range(len(word_lists_count_per_level)):
+                        word_lists_count_per_level[idx] = pd.concat(
+                            [word_lists_count_per_level[idx], word_lists_count_per_level_cam_mcq[idx]], ignore_index=True)
+                    line_plot_word_lists_count(word_lists_count_per_level, f"{dataset_name_param} | {model_name_param} | {prompt_id_param}", f"{dataset_name_param}_{model_name_param}_{prompt_id_param}", figsize=(3, 2.1))
