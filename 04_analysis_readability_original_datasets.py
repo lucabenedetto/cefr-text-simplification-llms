fd4c673c43e915be2d791939daaf4d7c3ddb1060
Luca Benedetto <luca.benedetto93@gmail.com>
2024-08-28 09:04:26 +0100

    refactor boxplot_readability_indexes into utils_plotting file

--- 04_analysis_readability_original_datasets.py
+++ 04_analysis_readability_original_datasets.py
@@ -1,12 +1,10 @@
 import matplotlib
-import matplotlib.pyplot as plt
 
 import pandas as pd
 from src.evaluators.readability import ReadabilityEvaluator
-from src.evaluators.constants import READABILITY_INDEXES
 from src.constants import COLUMN_TEXT_ID, COLUMN_TEXT, COLUMN_TEXT_LEVEL, CEFR_LEVELS
 from constants import CERD, CAM_MCQ
-
+from src.utils_plotting import boxplot_readability_indexes
 
 matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
@@ -20,25 +18,6 @@ def get_readability_indexes_per_target_level(df):
     read_idxs[COLUMN_TEXT_LEVEL] = read_idxs.apply(lambda r: target_level_dict[r[COLUMN_TEXT_ID]], axis=1)
     read_idxs_level = [read_idxs[read_idxs[COLUMN_TEXT_LEVEL] == level] for level in CEFR_LEVELS]
     return read_idxs_level
-
-
-def boxplot_readability_indexes(read_idxs_level, title, filename=None):
-    for readability_index in READABILITY_INDEXES:
-        fig, ax = plt.subplots(figsize=(6, 4.2))
-        ax.boxplot([local_df[readability_index] for local_df in read_idxs_level])
-        # ax.violinplot([local_df['flesch_kincaid_grade_level'] for local_df in read_idxs_level[1:]], showmeans=False, showmedians=True)
-        ax.set_title(f"{readability_index} | {title}")
-        ax.set_xticks(range(1, len(CEFR_LEVELS)+1))
-        ax.set_xticklabels(CEFR_LEVELS)
-        ax.grid(axis='y')
-        # this is to use if I want the num. of texts per level in the plot.
-        # ax.set_xticklabels([cefr + f'\n(n.={len(read_idxs_level[i])})' for i, cefr in enumerate(CEFR_LEVELS)])
-        plt.tight_layout()
-        if filename is None:
-            plt.show()
-        else:
-            plt.savefig(f'output_figures/boxplot_readability_indexes/{filename}_{readability_index}.png')
-        plt.close(fig)
 
 
 def main():
---
+++ src/utils_plotting.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+
+from src.constants import CEFR_LEVELS
+from src.evaluators.constants import READABILITY_INDEXES
+
+
+def boxplot_readability_indexes(read_idxs_level, title, filename=None):
+    for readability_index in READABILITY_INDEXES:
+        fig, ax = plt.subplots(figsize=(6, 4.2))
+        ax.boxplot([local_df[readability_index] for local_df in read_idxs_level])
+        # ax.violinplot([local_df['flesch_kincaid_grade_level'] for local_df in read_idxs_level[1:]], showmeans=False, showmedians=True)
+        ax.set_title(f"{readability_index} | {title}")
+        ax.set_xticks(range(1, len(CEFR_LEVELS)+1))
+        ax.set_xticklabels(CEFR_LEVELS)
+        ax.grid(axis='y')
+        # this is to use if I want the num. of texts per level in the plot.
+        # ax.set_xticklabels([cefr + f'\n(n.={len(read_idxs_level[i])})' for i, cefr in enumerate(CEFR_LEVELS)])
+        plt.tight_layout()
+        if filename is None:
+            plt.show()
+        else:
+            plt.savefig(f'output_figures/boxplot_readability_indexes/{filename}_{readability_index}.png')
+        plt.close(fig)
