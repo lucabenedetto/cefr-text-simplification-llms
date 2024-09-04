361a0473cb885e0d99aff313c6db7d01e6cb5679
Luca Benedetto <luca.benedetto93@gmail.com>
2024-09-04 10:24:58 +0100

    make MODEL_NAME_TO_STR and PROMPT_ID_TO_STR constants in the correct file

--- constants.py
+++ constants.py
@@ -1,3 +1,17 @@
 # Dataset names
 CAM_MCQ = 'cam_mcq'
 CERD = 'cerd'
+
+MODEL_NAME_TO_STR = {
+    'gemma_2b': 'Gemma 2B',
+    'gemma_7b': 'Gemma 7B',
+    'llama3_8b': 'Llama 3 8B',
+    'gpt_4o_240806': 'GPT-4o',
+    'gpt_4o_mini_240718': 'GPT-4o-mini',
+}
+PROMPT_ID_TO_STR = {
+    '01': 'a.1',
+    '02': 'a.2',
+    '11': 'b.1',
+    '12': 'b.2',
+}
