2900e1d187e33407ca67541a8854d99e7b0ae7b6
Luca Benedetto <luca.benedetto93@gmail.com>
2025-09-02 08:22:00 +0100

    add comment with instruction about how to add API keys

--- utils.py
+++ utils.py
@@ -16,13 +16,12 @@ def get_dataset(dataset_name: str) -> pd.DataFrame:
 
 def get_key_from_model_name(model_name: str) -> str:
     if model_name in OPENAI_MODEL_NAMES:
-        with open('/home/luca/.keys/openai_key.json', 'r') as f:
+        with open('~/.keys/openai_key.json', 'r') as f:  # TODO add path to your api keys
             data = json.load(f)
         api_key = data['key']
         return api_key
     if model_name in PRETRAINED_MODEL_NAMES:
-        # with open('/home/luca/.keys/hf_access_token.json', 'r') as f:  # This is for my local machine
-        with open('/home/lb990/.keys/hf_access_token.json', 'r') as f:  # This is for the server
+        with open('~/.keys/hf_access_token.json', 'r') as f:  # TODO add path to your api keys
             data = json.load(f)
         access_token = data['key']
         return access_token
